493c272591801fe0a74633f47a2540314d38c339
Kaung Set <kyawkaungset@gmail.com>
2021-06-24 09:18:00 +0630

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
+
+
+Route::get('/',[ArticleController::class, 'index']);
+
+Route::get('/articles',[ArticleController::class, 'index']);
+
+
+Route::get('/articles/detail/{id}',[ArticleController::class, 'detail']);
+
+Route::get('/articles/add',[ArticleController::class, 'add']);
+Route::post('/articles/add',[ArticleController::class, 'create']);
+Route::get('/articles/delete/{id}',[ArticleController::class, 'delete']);
+
+Route::post('/comments/add',[CommentController::class, 'create']);
+Route::get('/comments/delete/{id}',[CommentController::class, 'delete']);
+
+
+
+
+// Route::get('/articles/detail', function () {
+//     return 'Article Detail';
+// })->name('article.detail');
+
+// Route::get('/articles/more', function () {
+//     return redirect()->route('article.detail');
+// });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
